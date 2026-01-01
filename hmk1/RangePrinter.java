4f45de26c9630481d8f158c7400d8364b1ed62ed
Sarp Onaran <19soft1033@isik.edu.tr>
2026-01-01 20:27:06 +0300

    satisfying requirement change two

--- hmk1/RangePrinter.java
+++ hmk1/RangePrinter.java
@@ -9,7 +9,9 @@ public class RangePrinter {
 
         if (a < b) {
             for (int i = a + 1; i < b; i++) {
+               if(i % 2 != 0) {
                 System.out.println(i);
+              }
             }
         } else {
             for (int i = b + 1; i < a; i++) {

84b56f62e73a30274da07a7889d0d1f47b4a8a44
Sarp Onaran <19soft1033@isik.edu.tr>
2026-01-01 20:09:21 +0300

    satisfying requirement one

---
+++ hmk1/RangePrinter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RangePrinter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a < b) {
+            for (int i = a + 1; i < b; i++) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = b + 1; i < a; i++) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}
+
